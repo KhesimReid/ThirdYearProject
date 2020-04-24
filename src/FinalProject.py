4b51e5608448a5e2d5dcb411ae395aa973b678d1
KhesimReid <khesim.reid@gmail.com>
2020-04-24 18:24:08 +0100

    Reverted to original functions because changes greatly reduced the correctness of system output.

--- src/FinalProject.py
+++ src/FinalProject.py
@@ -30,9 +30,10 @@ def getAnswers(rawQuestion, documents, chosenMode):
     # Pre-process the user's question.
     question = QAFunctions.removePunctuation(rawQuestion)
     questionWords = question.split()
-    questionWords = QAFunctions.caseFold(questionWords)
     questionWords = QAFunctions.removeStopWords(questionWords)
-    questionWords = QAFunctions.stemWords(questionWords)
+    # questionWords = QAFunctions.caseFold(questionWords)
+    # questionWords = QAFunctions.removeStopWords(questionWords)
+    # questionWords = QAFunctions.stemWords(questionWords)
 
     # Dictionary to store files and the paragraphs which were sufficiently relevant
     relevantParagraphs = dict()
@@ -60,9 +61,10 @@ def getAnswers(rawQuestion, documents, chosenMode):
             # Pre-process the paragraph string
             cleanedText = QAFunctions.removePunctuation(paragraph)
             tokens = cleanedText.split()
-            tokens = QAFunctions.caseFold(tokens)
             tokens = QAFunctions.removeStopWords(tokens)
-            tokens = QAFunctions.stemWords(tokens)
+            # tokens = QAFunctions.caseFold(tokens)
+            # tokens = QAFunctions.removeStopWords(tokens)
+            #tokens = QAFunctions.stemWords(tokens)
 
             allText = [tokens, questionWords]
 
