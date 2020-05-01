ea9e49b0de8742a03d02a949b3307e7918b1d1c0
KhesimReid <khesim.reid@gmail.com>
2020-05-01 13:34:18 +0100

    Removed early exploratory files from remote repository.

--- src/ModelLoader.py
+++ src/ModelLoader.py
@@ -2,6 +2,7 @@ import pickle
 import gensim.downloader as api
 
 # Load the fasttext model then save it to a file, so that it can be used without having to load each time.
+print("Saving Fasttext Model...")
 model = api.load('fasttext-wiki-news-subwords-300')
 filename = 'fasttext_model.sav'
 pickle.dump(model, open(filename, 'wb'))
